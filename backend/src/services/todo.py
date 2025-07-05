f89ad0bea3f9658746c5c84731eb8f1a6c2fb048
mikolajm190 <mikolaj.mo158+github@gmail.com>
2025-07-05 22:07:02 +0200

    add todo service

---
+++ backend/src/services/todo.py
@@ -0,0 +1,48 @@
+from uuid import UUID
+
+from fastapi import Depends, HTTPException, status
+
+from src.models.todo import Todo
+from src.repositories.todo import TodoDAO
+from src.schemas.todo.create_todo_request import TodoCreateDTO
+from src.schemas.todo.update_todo_request import TodoUpdateDTO
+from src.schemas.todo.todo_response import TodoDTO
+
+
+class TodoService:
+    """Class for implementing business logic of todos."""
+
+    def __init__(self, dao: TodoDAO = Depends()) -> None:
+        self.dao = dao
+
+    def get_todos(self, limit: int, offset: int) -> list[Todo]:
+        return self.dao.get_all(limit, offset)
+    
+    def get_todo(self, todo_id: UUID) -> Todo:
+        todo = self.dao.get_by_id(todo_id)
+        if not todo:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Not found"
+            )
+        return todo
+    
+    def create_todo(self, body: TodoCreateDTO) -> Todo:
+        return self.dao.create(**body.model_dump())
+        
+    def update_todo(self, todo_id: UUID, body: TodoUpdateDTO) -> Todo:
+        todo = self.dao.update(todo_id, **body.model_dump())
+        if not todo:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Not found"
+            )
+        return todo
+    
+    def delete_todo(self, todo_id: UUID) -> None:
+        is_deleted = self.dao.delete(todo_id)
+        if not is_deleted:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Not found"
+            )
