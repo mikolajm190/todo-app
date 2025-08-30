b2b9eabef2b6bcb2836bb67c1e61146710da7172
mikolajm190 <mikolaj.mo158+github@gmail.com>
2025-08-30 20:22:15 +0200

    feat(auth): secure todo endpoints

--- backend/src/api/todos.py
+++ backend/src/api/todos.py
@@ -3,9 +3,11 @@ from uuid import UUID
 
 from fastapi import APIRouter, Depends, Query
 
+from src.api.auth import AuthServiceDep
 from src.schemas.todo.create_todo_request import TodoCreateDTO
 from src.schemas.todo.todo_response import TodoDTO
 from src.schemas.todo.update_todo_request import TodoUpdateDTO
+from src.services.auth import AuthTokenDep
 from src.services.todo import TodoService
 
 TodoServiceDep = Annotated[TodoService, Depends()]
@@ -13,33 +15,76 @@ TodoServiceDep = Annotated[TodoService, Depends()]
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/", responses={401: {"description": "Not authenticated"}})
 def read_todos(
-    service: TodoServiceDep,
+    todo_service: TodoServiceDep,
+    auth_service: AuthServiceDep,
+    token: AuthTokenDep,
     done: Annotated[bool | None, Query()] = None,
     limit: Annotated[int, Query(gt=0)] = 9,
     offset: Annotated[int, Query(ge=0)] = 0,
 ) -> list[TodoDTO]:
-    return service.get_todos(limit, offset, done)
+    auth_service.validate_token(token)
+    return todo_service.get_todos(limit, offset, done)
 
 
-@router.get("/{todo_id}", responses={404: {"description": "Not found"}})
-def read_todo(todo_id: UUID, service: TodoServiceDep) -> TodoDTO:
-    return service.get_todo(todo_id)
+@router.get("/{todo_id}", responses={
+    401: {"description": "Not authenticated"},
+    404: {"description": "Not found"}
+})
+def read_todo(
+    todo_id: UUID,
+    todo_service: TodoServiceDep,
+    auth_service: AuthServiceDep,
+    token: AuthTokenDep
+) -> TodoDTO:
+    auth_service.validate_token(token)
+    return todo_service.get_todo(todo_id)
 
 
-@router.post("/", status_code=201)
-def create_todo(body: TodoCreateDTO, service: TodoServiceDep) -> TodoDTO:
-    return service.create_todo(body)
+@router.post(
+    "/",
+    status_code=201,
+    responses={401: {"description": "Not authenticated"}}
+)
+def create_todo(
+    body: TodoCreateDTO,
+    todo_service: TodoServiceDep,
+    auth_service: AuthServiceDep,
+    token: AuthTokenDep
+) -> TodoDTO:
+    auth_service.validate_token(token)
+    return todo_service.create_todo(body)
 
 
-@router.put("/{todo_id}", responses={404: {"description": "Not found"}})
-def update_todo(todo_id: UUID, body: TodoUpdateDTO, service: TodoServiceDep) -> TodoDTO:
-    return service.update_todo(todo_id, body)
+@router.put(
+    "/{todo_id}",
+    responses={
+        401: {"description": "Not authenticated"},
+        404: {"description": "Not found"}
+    }
+)
+def update_todo(
+    todo_id: UUID,
+    body: TodoUpdateDTO,
+    todo_service: TodoServiceDep,
+    auth_service: AuthServiceDep,
+    token: AuthTokenDep
+) -> TodoDTO:
+    auth_service.validate_token(token)
+    return todo_service.update_todo(todo_id, body)
 
 
 @router.delete(
-    "/{todo_id}", status_code=204, responses={404: {"description": "Not found"}}
+    "/{todo_id}",
+    status_code=204,
+    responses={404: {"description": "Not found"}}
 )
-def delete_todo(todo_id: UUID, service: TodoServiceDep) -> None:
-    return service.delete_todo(todo_id)
+def delete_todo(
+    todo_id: UUID,
+    todo_service: TodoServiceDep,
+    auth_service: AuthServiceDep,
+    token: AuthTokenDep
+) -> None:
+    auth_service.validate_token(token)
+    return todo_service.delete_todo(todo_id)
